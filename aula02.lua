50766d1bf2bf4531582e68b27694f068d03c34c4
Júnia Camargo <95503135+juniatech@users.noreply.github.com>
2021-12-30 03:17:39 -0300

    Add files via upload

---
+++ aula02.lua
@@ -0,0 +1,29 @@
+-- aula 02 - variaveis, strings e concatenação
+
+-- criando variavel e substituindo por outra
+olaMundo = "Ola Mundo"
+olaMundo = "Olá Mundo!"
+
+-- imprimindo a variável
+print(olaMundo)
+
+-- imprimindo variavel/operação
+operacao = 45*10
+print(operacao)
+
+-- usando o sinal __ na variavel um_numero_qualquer
+um_numero_qualquer = 18
+print(um_numero_qualquer)
+
+-- concatenação/ imprimir as variaveis juntas com separação entre elas
+print(olaMundo .. " - ".. operacao .. " " .. um_numero_qualquer)
+
+--para concatenar variaveis com numeros inteiros
+num1 = 2
+num2 = 3
+
+print(num1 + num2)
+
+-- string = cadeia de caracteres
+-- fim da segunda aula
+-- juniatech
