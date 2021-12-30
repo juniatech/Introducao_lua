043188d48f433e16ca179b1124a2b462c35251df
Júnia Camargo <95503135+juniatech@users.noreply.github.com>
2021-12-30 02:42:43 -0300

    Add files via upload

---
+++ aula01.lua
@@ -0,0 +1,29 @@
+-- exibe o texto Olá Mundo na tela
+
+print("Olá Mundo!")
+
+-- exibe números diretamente
+print(123)
+
+-- exibe números em formato de texto
+print("123")
+
+--[[
+comentários
+pulando
+linhas...
+]]--
+
+-- operadores lógicos
+print(1+1)
+print(2-2)
+print(5*21)
+print(4/2)
+print(9%3)
+print(2>3)
+print(3>2)
+print(2>=3)
+print(2<=3)
+
+--final da primeira aula
+--juniatech
